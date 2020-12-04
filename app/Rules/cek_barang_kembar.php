b97c0bb9ce49974b890e62ec736cb1f0729da3fe
50162246 <maximillianhermawan@gmail.com>
2020-12-04 15:29:11 +0700

    update stock

---
+++ app/Rules/cek_barang_kembar.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Rules;
+use  App\barang;
+
+use Illuminate\Contracts\Validation\Rule;
+
+class cek_barang_kembar implements Rule
+{
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        //
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'The validation error message.';
+    }
+}
